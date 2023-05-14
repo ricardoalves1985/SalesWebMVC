dd2ae6aef3566e08aa545dc403f93fda4c2da9a2
RicardoAlves1985 <ricardoalves1985@gmail.com>
2023-05-13 23:09:06 -0300

    feat: implements simple search

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,32 @@ namespace SalesWebMVC.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordsServices _salesRecordsServices;
+
+        public SalesRecordsController(SalesRecordsServices salesRecordsServices)
+        {
+            _salesRecordsServices = salesRecordsServices;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime ? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyy-MM-dd");
+            var result = await _salesRecordsServices.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult GoupingSearch()
